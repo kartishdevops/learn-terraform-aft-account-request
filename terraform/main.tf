306e0329f6256d5ff6d8c8ddc2c049dfcdc0dd0f
Kartish <17425432+kartishr@users.noreply.github.com>
2023-06-26 17:36:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "Ball-Dev"
 }
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "kar2288@myyahoo.com"
+    AccountName               = "BallDev"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "karthi.eswar22@gmail.com"
+    SSOUserFirstName          = "kar"
+    SSOUserLastName           = "reddy"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "BallDev"
+}
