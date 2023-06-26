083578bdf6d47d1b5c6045919a17ab559e570c3a
Kartish <17425432+kartishr@users.noreply.github.com>
2023-06-26 15:14:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "2288kar@gmail.com"
+    AccountName               = "Ball-Dev"
+    ManagedOrganizationalUnit = "Ballot"
+    SSOUserEmail              = "eswar.karthis17@gmail.com"
+    SSOUserFirstName          = "Ball"
+    SSOUserLastName           = "Dev"
   }
 
   account_tags = {
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "Ball-Dev"
 }
